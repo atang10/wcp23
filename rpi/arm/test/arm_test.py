798c014e9771c8010034ed66b14fa491ed8870d1
Stefano Falero <sfalero1@binghamton.edu>
2020-03-08 17:39:59 -0400

    cameras not working because python needs to check if the keyboard is being used??????

--- rpi/arm/test/arm_test.py
+++ rpi/arm/test/arm_test.py
@@ -1,5 +1,27 @@
-from pyaxidraw import axidraw
-ad = axidraw.AxiDraw()
-ad.plot_setup("AxiDraw_API_v256/test/assets/AxiDraw_trivial.svg")
-ad.plot_run()
+from pyaxidraw import axidraw   # import module
+import signal
+import time
 
+def receiveAlarm(signum,stack):
+    #ts = time.time()
+    print("paused")
+    time.sleep(.1)
+    #timeElapsed = time.time()-ts
+   # signal.setitimer(signal.ITIMER_REAL,.1)
+    print("drawing")
+
+signal.signal(signal.SIGALRM, receiveAlarm)
+signal.setitimer(signal.ITIMER_REAL,.2,.2)
+
+ad = axidraw.AxiDraw()          # Initialize class
+ad.interactive()                # Enter interactive context
+ad.connect()                    # Open serial port to AxiDraw 
+                                # Absolute moves follow:
+ad.moveto(0,0)                  # Pen-up move to (1 inch, 1 inch)
+print("drawing")
+ad.lineto(8,0)                  # Pen-down move, to (2 inch, 1 inch)
+ad.lineto(8,8)
+ad.lineto(0,8)
+ad.lineto(0,0)
+ad.move(0,0)                  # Pen-up move, back to origin.
+ad.disconnect()                 # Close serial port to AxiDraw
--- rpi/test.py
+++ rpi/test.py
@@ -1,13 +1,62 @@
-from header import *
+from __future__ import print_function
+from imutils.video import VideoStream
+import numpy as np
+import datetime
+import imutils
+import cv2
+
+import signal
+import time
+from Arm import Arm
+from Cameras import Cameras
+#from Hardware import Hardware
+#from WebUI import WebUI
+#from ResultDiscrimination import ResultDiscrimination
 
 ## Initializing module objects: ##
-arm = Arm()
+#arm = Arm()
 #cameras = Cameras()
 #hardware = Hardware()
 #webUI = WebUI()
 
+def receiveAlarmCams(signum,stack):
+    global cameras
+    global running
+    if(running):
+#        ts = time.time()
+        frames = cameras.grabFramesEach()
+        cameras.showFramesEach(frames)
+#        network.sendFrames(feed1,freed2,feed3)
+#        timeElapsed = time.time()-ts
+#        signal.alarm(1/frameRate-timeElapsed) #timeElapsed must$
+#        print("frame callback: "+str(timeElapsed))
+    #also possible to ignore alarm during critical operations
+
+def terminate(objs):
+    for obj in objs:
+        obj.terminate()
+
+def startIntervalTimer(interval):
+# generates a signal every 1/frameRate seconds
+# intercepted w/ the callback recieveAlarm
+    signal.signal(signal.SIGALRM, receiveAlarmCams)
+    signal.setitimer(signal.ITIMER_REAL,interval,interval)
+
+
 running = True
 frameRate = 30 # fps
+cameras = Cameras()
 
-arm.drawPatternSVG("arm/AxiDraw_API_v256/test/assets/AxiDraw_trivial.svg")
+startIntervalTimer(1/frameRate)
 
+#frames = cameras.grabFramesEach()
+#cameras.showFramesEach(frames)
+
+while True:
+    # check to see if a key was pressed
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+cameras.terminate()
